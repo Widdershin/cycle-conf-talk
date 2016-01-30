4c5c77a4acdc7d3256ed7e7db4a378627a020a7b
Nick Johnstone <ncwjohnstone@gmail.com>
2016-01-30 17:36:44 +1300

    Now I have a little mario square

--- src/app.js
+++ src/app.js
@@ -1,8 +1,40 @@
-import {div} from '@cycle/dom';
+import {div, pre} from '@cycle/dom';
 import {Observable} from 'rx';
 
+function mario ({x, y, size}) {
+  return (
+    div('.mario', {
+      style: {
+        width: size + 'px',
+        height: size + 'px',
+        background: 'orange',
+        position: 'absolute',
+        left: x + 'px',
+        top: y + 'px'
+      }
+    })
+  );
+}
+
+function view (state) {
+  return (
+    div([
+      pre(JSON.stringify(state, null, 2)),
+      mario(state.mario)
+    ])
+  );
+}
+
 export default function App ({DOM, Animation}) {
+  const state$ = Observable.just({
+    mario: {
+      x: 50,
+      y: 50,
+      size: 20
+    }
+  });
+
   return {
-    DOM: Observable.just(div('.hello-world', 'Hello world'))
+    DOM: state$.map(view)
   };
 }
